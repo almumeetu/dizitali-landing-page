745cb20654fbf3fa63729263aaf4761641fd9f52
Al-Mumeetu Saikat <113462809+almumeetu@users.noreply.github.com>
2025-10-01 16:43:30 +0600

    update ladis bag landing page

---
+++ Ladis-bag/assets/js/main.js
@@ -0,0 +1,26 @@
+var swiper = new Swiper(".testimonial-yogurt", {
+    slidesPerView: 4,
+    spaceBetween: 24,
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    breakpoints: {
+        1199: {
+            slidesPerView: 4,
+        },
+        991: {
+            slidesPerView: 3,
+        },
+        767: {
+            slidesPerView: 2,
+        },
+        0: {
+            slidesPerView: 2,
+        }
+    }
+});
